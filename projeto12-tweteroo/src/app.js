d63ea714945bf43f88dfc37c062e0e6ee0dece1e
DiegoKoyama <yukioko.jp@gmail.com>
2023-01-08 13:58:09 -0300

    POST /tweets

--- projeto12-tweteroo/src/app.js
+++ projeto12-tweteroo/src/app.js
@@ -2,19 +2,45 @@ import express from "express";
 import cors from "cors";
 
 const server = express();
-const username = ""
+
 server.use(express.json());
 server.use(cors());
+
+const user = []
+const tweets = []
 
 const PORT = 5000;
 
 server.post("/sign-up", (req, res) =>{
-    const user = req.body
+    const date = req.body
+    user.push({...date})
     if(user.username === "" || user.avatar === ""){
         console.log("Preencha os dados solicitados!")
         return;
     }
-    res.send("ok")
+        res.send("ok")  
+})
+
+server.get("/sign-up", (req, res) => {
+    res.send(user)
+})
+
+server.post("/tweets", (req, res) => {
+    const date = req.body
+    let avatar = ""
+    user.find((item) => {if(item.username === date.username){
+        avatar = item.avatar
+    }})
+    if(user.find((element) => element.username === date.username)){
+        tweets.push({...date, avatar})
+        res.send("ok")
+        return;
+    }
+    res.sendStatus(401)
+})
+
+server.get("/tweets", (req, res) => {
+    res.send(tweets)
 })
 
 server.listen(PORT);
